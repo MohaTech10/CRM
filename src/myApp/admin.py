7c78db92f859c4472508159638cc60c91d73b08e
Developer <developer@MacBook-Air-alkhas-b-Mohammed.local>
2020-08-11 19:40:45 +0300

    db tables created

--- src/myApp/admin.py
+++ src/myApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+# from Project.settings import class/function
+
+admin.site.register(Customer)
+admin.site.register(Products)
--- src/myApp/models.py
+++ src/myApp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+
+    name = models.CharField(max_length=200, null=True)  #  false => required
+    phone = models.CharField(max_length=11, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+    # saving  the date that the object is created at
+
+
+    def __str__(self):
+        return self.name  # it must return string value
+
+
+
+
+class Products(models.Model):
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+'''
+    1- python3 manage.py makemigrations
+    2- python3 manage.py migrate
+    3- any property changing must be migrated
+
+
+
+'''
